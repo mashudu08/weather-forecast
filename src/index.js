b6aaa0fffd3dc9d4d4c7fc90e53b5f74511a63ef
Mashudu Luvhengo <mluvhengo25@gmail.com>
2024-02-27 11:31:22 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElemnent.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+  
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -56,7 +58,16 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function getForecast(city){
+    let apiKey = "a9da343b49eoea401ffefd616fa0c85t";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+} 
+
+function displayForecast(response) {
+    console.log(response.data);
+
+
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
@@ -84,5 +95,6 @@ let searchFormElememt = document.querySelector("#search-form");
 searchFormElememt.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Johannesburg");
-displayForecast();
+
+
 
